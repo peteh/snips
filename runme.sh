e532bdf508fede8ce5108aa73c8cb09456717599
Pete <git@kthxbye.us>
2018-12-21 22:55:12 +0100

    ADD: startup script

---
+++ runme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd hotword/beep
+lxterminal -e python3 beep.py
+cd ../../
+ 
+cd skills
+
+cd shutup
+lxterminal -e python3 shutup-skill.py
+cd ..
+
+cd joke
+lxterminal -e python3 joke-skill.py
+cd ..
+
+cd SuperSonic
+lxterminal -e sudo Debug/SuperSonic
